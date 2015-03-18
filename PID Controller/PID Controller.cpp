aface1d8894f48b826ebd9b1407cd36e753c8689
maxlazarus <max.prokopenko@gmail.com>
2015-03-18 01:33:49 -0700

    cleanup

--- PID Controller/PID Controller.cpp
+++ PID Controller/PID Controller.cpp
@@ -4,34 +4,31 @@
  * Created: 1/23/2015 9:28:17 AM
  *  Author: Max Prokopenko
  *  max@theprogrammingclub.com
- *
- *  NOPE
- *  Nano pinout:
- *	 D2:	Hall Effect input
- *   D6:	PWM0 out
  */ 
 
 #include "F_CPU.h" // clock speed
 
-#define CONTROL_PORT PORTD
-#define HCTL_BYTE_SELECT_BIT 2
-#define HCTL_ENABLE_BIT 7
-#define HCTL_SELECT 4
-#define MOTOR1 2 // soft serial
-#define MOTOR2 3 // PWM
-#define HCTL_CLK_BIT 6
+#define COM_SPEED 57600
+#define CONTROL_PORT			PORTD
+#define MOTOR1					2 // soft serial
+#define MOTOR2					3 // PWM
+#define HCTL_SELECT				4
+#define HCTL_BYTE_SELECT_BIT	5
+#define HCTL_CLK_BIT			6
+#define HCTL_ENABLE_BIT			7
 #define DATA_HIGH_NIBBLE (PINC << 4)
 #define DATA_LOW_NIBBLE PINB
 #define ADC10BIT ((analogHigh << 2) | (analogLow >> 6))
-#define ADC_TO_DOUBLE 0.000976562
+
 // conversion constants
+#define ADC_TO_DOUBLE 0.000976562 // 0 to 1.0
 #define PI 3.1415926
 #define TICKS_TO_RADIANS 0.0005236 // (2 * PI) / 12000
 #define RADIANS_TO_TICKS 1910 // 12000 / (2 * PI)
 #define ADC_TO_HALF_REV 5.859 // 6000 / 1024
 #define HALF_REV 6000
 #define SOFT_SERIAL_BAUDRATE 19200
-#define SOFT_SERIAL_DELAY (1000000 / SOFT_SERIAL_BAUDRATE)
+#define SOFT_SERIAL_DELAY (1000000 / SOFT_SERIAL_BAUDRATE) // us
 
 #include <avr/io.h>
 #include <math.h>
@@ -48,10 +45,11 @@
 void velocityStep(double d);
 void controlSequence();
 uint16_t getPosition();
-unsigned int TIM16_ReadTCNT1();
 void setBitTo(uint8_t bit, uint8_t value, volatile uint8_t *reg);
 void softSerial(uint8_t message);
-void testServoController();
+void startADC();
+void selectADC(uint8_t n);
+void blorp();
 
 //--------------CONTROLLER GAINS--------------//
 	
@@ -68,13 +66,10 @@ volatile uint8_t analogLow = 0, analogHigh = 0;
 uint16_t desiredPosition = HALF_REV;
 uint16_t lastPosition = 0, position = 0, zero = 0;
 int16_t velocity = 0;
-int32_t I = 0;
-int32_t P = 0, D = 0;
+int32_t P = 0, I = 0, D = 0;
 int32_t E = 0, lastE = 0;
 double speed;
-PWM externalClock(0);
-PWM controlCycle(1);
-PWM motor2(2);
+PWM clockOut(0), controlCycle(1), motor2(2); // timer0, 1, 2
 Button panelButton(5, &PINC);
 char str[20]; // debug output buffer
 double path[] = {1};
@@ -83,7 +78,7 @@ double path[] = {1};
 
 class Motor {
 	public:
-		// accepts a decimal between -1 (full reverse) and 1 (full forward)
+		// accepts between -1 (full reverse) and 1 (full forward)
 		static void setSpeed(double d) {
 			if(d > 1)
 				d = 1;
@@ -105,6 +100,7 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 ISR(ADC_vect) {
+	
 	analogLow = ADCL;
 	analogHigh = ADCH;
 }
@@ -113,36 +109,30 @@ ISR(ADC_vect) {
 
 int main(void) {
 	
-	DDRB = 0b11110000;	//B5 output: board LED
-	DDRD = 0b11111111;
-	PORTC = 0;
+	void (*foo)() = blorp;
+	foo = blorp;
+	foo();
+	
+	DDRB = 0b11110000;
 	DDRC = 0b00000000;
-	PORTD = 0xff;
-	
-	//-Ulfuse:w:0x22:m // argument for AVRdude to get clock out
-	
-	//USART_Init(convertBaud(19200)); // motor serial speed
-	USART_Init(convertBaud(57600)); // computer com speed
-	
-	ADMUX  = 0b01100110; // port A6 ADC selected
-	ADCSRA = 0b10001011; // on, 2x clock
-	ADCSRB = 0b00000000; // free running
+	DDRD = 0b11111111;
 
 	Kp = 0.4;
 	Ki = 0.04;
 	Kd = 0.2;
 	
+	selectADC(6);
+	USART_Init(convertBaud(COM_SPEED));
 	sei(); // enable global interrupts
 	
 	motor2.start();
-	externalClock.start();
+	clockOut.start();
 	
 	while(1) {
-		// both motors controlled by potentiometer
+		// both motors controlled by single potentiometer on A6
 		motor2.setDuty(ADC10BIT * ADC_TO_DOUBLE);
-		setBitTo(ADSC, 1, &ADCSRA); // ADC read start
-		Motor::setSpeed(ADC10BIT * ADC_TO_DOUBLE);
-		_delay_ms(50);
+		Motor::setSpeed(2 * ADC10BIT * ADC_TO_DOUBLE - 1);
+		startADC();
 	}
 	
 	controlCycle.start();
@@ -168,17 +158,28 @@ int main(void) {
 
 //------------------FUNCTIONS
 
+void blorp() {
+	
+}
+
+void selectADC(uint8_t n) {
+	
+		ADMUX  = 0b01100000 | n; // port A6 ADC selected
+		ADCSRA = 0b10001011; // on, 2x clock
+		ADCSRB = 0b00000000; // free running
+}
+
 uint16_t getPosition() {
 	
 	uint16_t count = 0;
 
-	externalClock.stop();
+	clockOut.stop();
 	
 	setBitTo(HCTL_BYTE_SELECT_BIT, 0, &CONTROL_PORT); // select MSB
 	setBitTo(HCTL_ENABLE_BIT, 0, &CONTROL_PORT); // enable
 	setBitTo(HCTL_CLK_BIT, 0, &CONTROL_PORT); // clock low
 	
-	count = (count << 8) | DATA_HIGH_NIBBLE | DATA_LOW_NIBBLE; // get byte
+	count = DATA_HIGH_NIBBLE | DATA_LOW_NIBBLE; // get byte
 	
 	setBitTo(HCTL_BYTE_SELECT_BIT, 1, &CONTROL_PORT); // select LSB
 
@@ -186,19 +187,8 @@ uint16_t getPosition() {
 	
 	setBitTo(HCTL_ENABLE_BIT, 1, &CONTROL_PORT); // disable
 	
-	externalClock.start();
+	clockOut.start();
 	return count;
-}
-
-unsigned int TIM16_ReadTCNT1( void ) {
-	unsigned char sreg;
-	unsigned int i;
-	sreg = SREG; // global interrupt flags saved
-	cli(); // disable interrupts
-	i = TCNT1;
-	SREG = sreg; // global interrupt flags restored
-	sei();
-	return i;
 }
 
 void setBitTo(uint8_t bit, uint8_t value, volatile uint8_t *reg) {
@@ -261,24 +251,9 @@ void controlSequence() {
 		USART_Send_string(str);
 	}
 	
-	setBitTo(ADSC, 1, &ADCSRA); // ADC read start
+	startADC();
 }
 
-void testServoController() {
-	uint16_t pulseWidth = 0;
-		
-	while(1) {
-		pulseWidth = 400 + 2 * ADC10BIT;
-		setBitTo(ADSC, 1, &ADCSRA); // ADC read start
-			
-		setBitTo(5, 1, &PORTD);
-		for(uint16_t i = 0; i < pulseWidth; i++){
-			_delay_us(1);
-		}
-		setBitTo(5, 0, &PORTD);
-		_delay_ms(10);
-			
-		sprintf(str, "%u,", pulseWidth);
-		USART_Send_string(str);
-	}
+void startADC() {
+	setBitTo(ADSC, 1, &ADCSRA); // ADC read start
 }
