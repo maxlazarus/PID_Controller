487ddaf13a2e484f3418ddb00ca7d3732208715b
maxlazarus <max.prokopenko@gmail.com>
2015-04-02 18:42:53 -0700

    integrated and functional

--- PID Controller/PID Controller.cpp
+++ PID Controller/PID Controller.cpp
@@ -11,6 +11,8 @@
 
 //--------------INCLUDES AND DEFINES-----------//
 
+// measuring linear conversion: 1191 = 100.6mm, 1239 = 149.2mm
+
 #include "F_CPU.h"				// clock speed
 #define COM_SPEED				57600
 #define CONTROL_PORT			PORTD
@@ -31,14 +33,15 @@
 
 #define ENCODERX				0
 #define ENCODERY				1
-#define DATA_HIGH_NIBBLE		(PINC << 4)
-#define DATA_LOW_NIBBLE			PINB
+#define DATA_HIGH_NIBBLE		((PINC & 0b00001111) << 4)
+#define DATA_LOW_NIBBLE			(PINB & 0b00001111)
 #define ADC10BIT				((analogHigh << 2) | (analogLow >> 6))
 
 // conversion constants
 #define ADC_TO_DOUBLE			(2 * ADC10BIT * 0.000976562) - 1 // -1.0 to 1.0
 #define PI						3.1415926
-#define WHEEL_HALF_REVOLUTION	1024 // 24000 for commercial
+#define SLIDE_TO_CM				0.01
+#define WHEEL_HALF_REVOLUTION	2048 // 24000 for commercial
 #define WHEEL_TO_RADIANS		PI / WHEEL_HALF_REVOLUTION
 #define WHEEL_TO_DEGREES		180.0 / WHEEL_HALF_REVOLUTION
 #define SOFT_SERIAL_BAUDRATE	19200
@@ -66,10 +69,14 @@ void getHighLowPosition(uint8_t encoder, volatile uint8_t* high, volatile uint8_
 void softSerial(uint8_t message);
 void startADC();
 void selectADC(uint8_t n);
-void home();
+void linearHome();
+void angularHome();
 void setMotor1Speed(float f);
+void setMotor3Speed(float f);
 void print_debug_info();
 void updateGlobals();
+void readMaster();
+void rectangularToPolar(float x, float y, volatile uint16_t* rOut, volatile uint16_t* thetaOut);
 
 //------------------GLOBAL FLAGS
 
@@ -79,10 +86,8 @@ volatile bool controllable = true;
 //------------------VARIABLES
 
 Output
-	/* 
 	alt1(			ALT1,			&ALT_PORT,		&DDRB), 
 	alt2(			ALT2,			&ALT_PORT,		&DDRB),
-	*/
 	encoderSelect(	HCTL_SELECT,	&CONTROL_PORT,	&DDRD),
 	encoderEnable(	HCTL_ENABLE,	&CONTROL_PORT,	&DDRD),
 	encoderClock(	HCTL_CLK,		&CONTROL_PORT,  &DDRD),
@@ -90,14 +95,15 @@ Output
 	motorSerial(	MOTOR1,			&CONTROL_PORT,	&DDRD)
 	;
 Input 
-	panelButton(	ALT2,			&PINC,			&DDRB);
+	panelButton(	5,				&PINC,			&DDRC);
 PID angular, linear;
 
-volatile uint8_t analogLow = 0, analogHigh = 0;
-uint16_t desiredTheta = PI / WHEEL_TO_RADIANS, desiredR = 0;
-
+volatile uint8_t draw;
+volatile uint16_t count;
+volatile uint8_t analogLow, analogHigh;
 volatile uint16_t left, right;
-volatile float xIn = 0, yIn = 0;
+volatile float xIn, yIn;
+volatile uint16_t desiredTheta, desiredR;
 volatile uint16_t r, lastR, theta, lastTheta;
 volatile int16_t angularV, linearV;
 volatile float speed;
@@ -135,65 +141,24 @@ int main(void) {
 	clockOut.start();
 	controllable = true;
 	
-	angular.setGains(1, 0, 0);
-	linear.setGains(1, 0, 0);
-	// home();
-	
-	volatile uint8_t high, low;
-	
-	while(1) {
-		getHighLowPosition(ENCODERX, &high, &low);
-		cli();
-		sprintf(str, "high: %d, low: %d\n", (uint16_t)(high), (uint16_t)(low));
-		USART_Send_string(str);
-		_delay_ms(100);
-		sei();
-	}
-	
-	while(1) {
-		selectADC(ADC_LEFT);
-		startADC();
-		_delay_ms(100);
-		left = ADC10BIT;
-	
-		selectADC(ADC_RIGHT);
-		startADC();
-		_delay_ms(100);
-		right = ADC10BIT;
-		
-		sprintf(str, "L: %d, R: %d\n", left, right);
-		USART_Send_string(str);
-		
-		translate(left, right, &xIn, &yIn);
-		
-		sprintf(str, "X: %d, Y: %d\n", (int16_t)(xIn), (int16_t)(yIn));
-		USART_Send_string(str);
-	}
-	
-	while(0) {
-		// both motors controlled by single potentiometer on A6
-		motor2.setSpeed(1);
-		for(int i = 0; i < 20; i++) {
-			updateGlobals();
-			startADC();
-			_delay_ms(100);
-			sprintf(str, "%d,", angularV);
-			USART_Send_string(str);
-		}
-		motor2.setSpeed(-1);
-		for(int i = 0; i < 20; i++) {
-			updateGlobals();
-			startADC();
-			_delay_ms(100);
-			sprintf(str, "%d,", angularV);
-			USART_Send_string(str);
-		}
-	}
+	angular.setGains(2, 0, 1);
+	linear.setGains(1, 0, 1);
+	linearHome();
 	
 	controlCycle.start();
 	
 	while(1) {
-		debug = true;
+		debug = false;
+		
+		sprintf(str,
+			"%u %u %d\n",
+			(uint16_t)(desiredR),
+			(uint16_t)(desiredTheta),
+			draw
+			);
+		USART_Send_string(str);
+		
+		_delay_ms(100);
 	}
 }
 
@@ -208,17 +173,57 @@ void setMotor1Speed(float f) {
 	softSerial((uint8_t)(192 + (f * 63.5)));
 }
 
-void home() {
+void setMotor3Speed(float f) {
+	// accepts between -1 (full reverse) and 1 (full forward)
+	if(f > 1)
+	f = 1;
+	else if(f < -1)
+	f = -1;
+	softSerial((uint8_t)(64 + (f * 63.5)));
+}
+
+void linearHome() {
+	
+	bool homed = false;
+	uint32_t currentPosition = 32000;
+	while(!homed) {
+		setMotor1Speed(-0.5f);
+		motor2.setSpeed(0);
+		currentPosition = getPosition(ENCODERX);
+		if(currentPosition == 0) {
+			homed = true;
+		}
+	}
+}
+
+void angularHome() {
 	
 	bool homed = false;
 	uint32_t currentPosition = 32000;
 	while(!homed) {
 		motor2.setSpeed(-0.5f);
-		currentPosition = getPosition(ENCODERX);
+		currentPosition = getPosition(ENCODERY);
 		if(currentPosition > 65000 || currentPosition == 0) {
 			homed = true;
 		}
 	}
+}
+
+void readMaster() {
+	
+	selectADC(ADC_LEFT);
+	startADC();
+	_delay_ms(1);
+	left = ADC10BIT;
+		
+	selectADC(ADC_RIGHT);
+	startADC();
+	_delay_ms(1);
+	right = ADC10BIT;
+		
+	draw = (panelButton.isDown())? 1 : 0;
+		
+	translate(left, right, &xIn, &yIn);
 }
 
 void selectADC(uint8_t n) {
@@ -230,7 +235,7 @@ void selectADC(uint8_t n) {
 
 uint16_t getPosition(uint8_t encoder) {
 	
-	uint16_t count;
+	uint16_t count = 0;
 	
 	setBitTo(HCTL_SELECT, encoder, &CONTROL_PORT);
 
@@ -249,31 +254,7 @@ uint16_t getPosition(uint8_t encoder) {
 	setBitTo(HCTL_ENABLE, 1, &CONTROL_PORT); // disable
 	
 	clockOut.start();
-	
 	return count;
-}
-
-void getHighLowPosition(uint8_t encoder, volatile uint8_t* high, volatile uint8_t* low) {
-	
-	setBitTo(HCTL_SELECT, encoder, &CONTROL_PORT);
-
-	clockOut.stop();
-	
-	setBitTo(HCTL_BYTE, 0, &CONTROL_PORT); // select MSB
-	setBitTo(HCTL_ENABLE, 0, &CONTROL_PORT); // enable
-	setBitTo(HCTL_CLK, 0, &CONTROL_PORT); // clock low
-	
-	// count = DATA_HIGH_NIBBLE | DATA_LOW_NIBBLE; // get byte
-	*high = (uint8_t) DATA_HIGH_NIBBLE | DATA_LOW_NIBBLE;
-	
-	setBitTo(HCTL_BYTE, 1, &CONTROL_PORT); // select LSB
-
-	// count = count << 8 | DATA_HIGH_NIBBLE | DATA_LOW_NIBBLE; // get byte
-	*low = (uint8_t) DATA_HIGH_NIBBLE | DATA_LOW_NIBBLE;
-	
-	setBitTo(HCTL_ENABLE, 1, &CONTROL_PORT); // disable
-	
-	clockOut.start();
 }
 
 void updateGlobals() {
@@ -282,11 +263,12 @@ void updateGlobals() {
 	r = getPosition(ENCODERX);
 	linearV = r - lastR;
 	
-	_delay_ms(1);
-	
 	lastTheta = theta;
-	theta = getPosition(ENCODERY) % 2048;
-	angularV = theta - lastTheta;
+	theta = getPosition(ENCODERY) % 4096;
+	if(lastTheta > 2048 && theta < 2048)
+		angularV = 4096 + theta - lastTheta;
+	else
+		angularV = theta - lastTheta;
 }
 
 void softSerial(uint8_t message) {
@@ -318,28 +300,34 @@ void velocityStep(float f) {
 
 void controlSequence() {
 	
-	// alt1.set();
-	
-	if(controllable) {
-		desiredTheta = (uint16_t)(WHEEL_HALF_REVOLUTION * (1 + ADC_TO_DOUBLE));
-	}
+	alt1.set();
 	
 	updateGlobals();
+	readMaster();
+	
+	desiredR = 1150 - (uint16_t)(sqrt(xIn * xIn + yIn * yIn) / (4 * SLIDE_TO_CM));
+	desiredTheta = (uint16_t)((4096 * (atan2(xIn, -yIn) + PI)) / (2 * PI));
+	//	rectangularToPolar(xIn, yIn, &desiredR, &desiredTheta);
+	/*
+	if(draw == 1)
+		desiredTheta = 1024;
+	else
+		desiredTheta = 3072;
+	*/	
 		
+	speed = linear.compute(desiredR, r) * SLIDE_TO_CM;
+	setMotor1Speed(speed);
+	
 	speed = angular.compute(desiredTheta, theta) * WHEEL_TO_RADIANS;
 	motor2.setSpeed(speed);
 	
-	// alt2.set();
-
-	if(debug) {
-		// print_debug_info();
-	}
+	alt2.set();
 	
-	// alt2.clear();
+	alt2.clear();
 	
 	startADC();
 	
-	// alt1.clear();
+	alt1.clear();
 }
 
 void startADC() { setBitTo(ADSC, 1, &ADCSRA); }
@@ -348,8 +336,13 @@ void print_debug_info() {
 	
 	sprintf(str,
 		"d: %u, p: %u\n",
-		(uint16_t)(desiredTheta * WHEEL_TO_DEGREES),
-		(uint16_t)(theta * WHEEL_TO_DEGREES)
+		(uint16_t)(desiredTheta/* * WHEEL_TO_DEGREES*/),
+		(uint16_t)(theta/* * WHEEL_TO_DEGREES*/)
 	);
 	USART_Send_string(str);
+}
+
+void rectangularToPolar(float x, float y, volatile uint16_t* rOut, volatile uint16_t* thetaOut) {
+	*rOut = (uint16_t)(512 - (sqrt(x * x + y * y) * SLIDE_TO_CM));
+	*thetaOut = (uint16_t)(atan2(x, y) / WHEEL_TO_RADIANS);
 }
